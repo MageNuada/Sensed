54193a6c85ec7682a0ee5a463719f09deb319824
MageNuada <nuada.sunriser@gmail.com>
2023-02-09 03:46:42 +0300

    Added model.

---
+++ Sensed/Models/Models.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sensed.Models
+{
+    public class Account
+    {
+        private readonly IDataProvider _provider;
+
+        public Account(AccountDTO dto, IDataProvider provider)
+        {
+            _provider = provider;
+            Id = dto.Id;
+            Name = dto.Name;
+            Description = dto.Description;
+            Photos = dto.PhotosIds.Select(x =>
+            new Lazy<Task<Bitmap>>(async() => await
+            _provider.GetPhoto(x))).ToList();
+        }
+
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public List<Lazy<Task<Bitmap>>> Photos { get; set; } = new();
+    }
+}
