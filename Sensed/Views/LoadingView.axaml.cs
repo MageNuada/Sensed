9a9388fe31f70fe521ae7618e0c83b70071e04ff
MageNuada <nuada.sunriser@gmail.com>
2023-02-09 03:46:21 +0300

    Added loading view.

---
+++ Sensed/Views/LoadingView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Sensed.Views
+{
+    public partial class LoadingView : UserControl
+    {
+        public LoadingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
